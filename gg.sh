d42fc5bebbabac69bccbdf351c6e6076ff9827a3
WeibinMeng <m_weibin@163.com>
2019-01-15 20:24:35 +0800

    change_git_shell

--- gg.sh
+++ gg.sh
@@ -3,10 +3,11 @@
 # * Author        : Weibin Meng
 # * Email         : m_weibin@163.com
 # * Create time   : 2018-10-18 13:18
-# * Last modified : 2018-10-18 13:18
+# * Last modified : 2019-01-15 20:24
 # * Filename      : gg.sh
 # * Description   : 
 # **********************************************************
+git pull origin master
 git add .
 git commit -m "$1"
 git push origin master
